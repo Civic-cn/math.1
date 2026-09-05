8f5c51723e0ff20b1332b6171f9a03fed0f0cc60
wcj2913522514 <2913522514@qq.com>
2026-09-06 02:02:23 +0800

    使用fastapi重写api

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,28 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import json
+#get 和post方法都写在这里 
+from fastapi import FastAPI
+from pydantic import BaseModel #专门管数据的解析和校验
+
+app = FastAPI()
 
 profile = {
     "heroTitle": "关于我",
     "heroSubtitle": "项目，创意，灵感，心得，我的作品",
 }
+class AnalyzeRequest(BaseModel):
+    text: str
+#规定analyze的格式要求是str basemodel继承了str的格式给到analyze
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print(self.headers)          # 打印收到的请求头
-        print(self.client_address)   #打印 客户ip
-        if self.path == "/api/profile":
-            self.send_response(200) #状态头
-            self.send_header("Content-Type", "application/json")#响应头
-            self.end_headers()#空行
-            body = json.dumps(profile, ensure_ascii=False)  # 响应体 ensure_ascii=False：让中文原样输出
-            self.wfile.write(body.encode("utf-8"))
+@app.get("/api/profile")
+def get_profile():
+    return profile
 
-        else:
-            self.send_response(404)
-            self.end_headers()
+# 框架把handmake里面的4对细节都封装了， 请求头 请求体 空行啊，状态头啊，状态体啊，空行啊
 
-print("后端已启动：http://localhost:8000/api/profile")
-HTTPServer(("", 8000), Handler).serve_forever()
-
-#一个守在8000端口上等待调用方服务的进程
+@app.post("/api/analyze")
+def analyze(req: AnalyzeRequest):
+    return {
+        "text": req.text,
+        "score": 0.5,
+        "label": "偏平静",
+        "pinyin": "（模块 6 再说）",
+    }
